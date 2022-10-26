9b506f1b45753574d23a0be2b1ba198cd0137ee6
Refael Abramov <refaelavramov@gmail.com>
2022-10-26 14:49:11 +0300

    functionality

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -13,27 +13,50 @@ function onInit() {
     addListeners()
 }
 
+function renderMeme() {
+    const meme = getMeme()
+    // image
+    gCtx.drawImage(meme.url, 0, 0, gElCanvas.width, gElCanvas.height)
+    // text
+    const lines = meme.lines
+
+    lines.forEach(({ color, size, txt, pos, align }) => {
+        gCtx.fillStyle = color
+        gCtx.font = `${size}px impact`
+        gCtx.fillText(txt, gElCanvas.width / 2, pos * gElCanvas.height) // Draws (fills) a given text at the given (x, y) position.
+        gCtx.strokeText(txt, gElCanvas.width / 2, pos * gElCanvas.height) // Draws (fills) a given text at the given (x, y) position.
+        gCtx.textAlign = align
+    })
+
+}
+
 function onDrawText(text) {
     setLineTxt(text)
-    gCtx.lineWidth = 1
-    gCtx.strokeStyle = 'brown'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '40px Arial'
+    gCtx.strokeWidth = 1
+    gCtx.strokeStyle = 'black'
     renderMeme()
 
 }
 
-function renderMeme() {
+function onSwitchLine() {
+    const lineIdx = switchLine()
     const meme = getMeme()
-    const text = meme.lines[0].txt
-    // image
-    gCtx.drawImage(meme.url, 0, 0, gElCanvas.width, gElCanvas.height)
-    // text
-    gCtx.fillText(text, 50, 50) // Draws (fills) a given text at the given (x, y) position.
-    gCtx.strokeText(text, 50, 50) // Draws (strokes) a given text at the given (x, y) position.
+    document.querySelector('.text-input').value = meme.lines[lineIdx].txt
+}
+
+function onChangeSize(size) {
+    changeSize(size)
+    renderMeme()
+}
+
+function onChangeColor(color) {
+    changeColor(color)
+    renderMeme()
 }
 
 function onImgSelect() {
+    document.querySelector('.meme-editor-container').classList.remove('hide')
+    document.querySelector('.main-content').classList.add('hide')
     setImg(this)
     renderMeme()
 }
--- js/service/meme-service.js
+++ js/service/meme-service.js
@@ -5,27 +5,53 @@ var gImgs = createImgs()
 var gMeme = {
     selectedImgId: 0,
     selectedLineIdx: 0,
-    url:'',
+    url: '',
     lines: [
         {
             txt: '',
-            size: 20,
-            align: 'left',
-            color: 'red'
+            size: 30,
+            align: 'center',
+            color: 'white',
+            pos:0.2
+        },
+        {
+            txt: '',
+            size: 30,
+            align: 'center',
+            color: 'white',
+            pos:0.9
         }
     ]
 }
 
-function setLineTxt(txt){
-    gMeme.lines[0].txt = txt
+function setLineTxt(txt) {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].txt = txt
 }
 
-function setImg(elImg){
+function switchLine() {
+    gMeme.selectedLineIdx++
+    if (gMeme.selectedLineIdx === gMeme.lines.length) gMeme.selectedLineIdx = 0
+    return gMeme.selectedLineIdx
+}
+
+function changeSize(size) {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].size += size
+
+}
+
+function changeColor(color) {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].color = color
+}
+
+function setImg(elImg) {
     gMeme.selectedImgId = elImg.dataset.id
     gMeme.url = elImg
 }
 
-function getImgs(){
+function getImgs() {
     return gImgs
 }
 
