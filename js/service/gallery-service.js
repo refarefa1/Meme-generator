a42ccc4e166b4331da8ab637b30b8b279ca1c0d9
Refael Abramov <refaelavramov@gmail.com>
2022-10-27 15:00:18 +0300

    fix bugs

--- js/service/gallery-service.js
+++ js/service/gallery-service.js
@@ -7,7 +7,7 @@ var gMemes
 function createImgs() {
     let imgs = []
     for (let i = 1; i < 17; i++) {
-        const img = createImg(`http://127.0.0.1:5500/img/memes/${i}.jpg`)
+        const img = createImg(`../../img/memes/${i}.jpg`)
         imgs.push(img)
     }
     return imgs
